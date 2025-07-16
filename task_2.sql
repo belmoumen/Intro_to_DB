39c3707df91073eb5c68bc593713d510a4881273
Mehdi Belmoumen <belmoumen.meh@gmail.com>
2025-07-16 11:07:45 +0100

    add task 2 file

--- task_2.sql
+++ task_2.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS Authors (
 );
 
 -- Create the BOOKS table
-CREATE TABLE IF NOT EXISTS books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
@@ -16,15 +16,15 @@ CREATE TABLE IF NOT EXISTS books (
     FOREIGN KEY (author_id) REFERENCES authors(author_id)
 );
 
--- Create the CUSTOMERS table
-CREATE TABLE IF NOT EXISTS customers (
+-- Create the CUSTOMERS Table
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215),
     address TEXT
 );
 
--- Create the ORDERS table
+-- Create the ORDERS Table
 CREATE TABLE IF NOT EXISTS orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS orders (
 );
 
 -- Create the ORDER_DETAILS table
-CREATE TABLE IF NOT EXISTS order_details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,

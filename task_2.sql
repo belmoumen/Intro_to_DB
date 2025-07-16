27017623c741f08f25ef1b01dd017a647d1a6964
Mehdi Belmoumen <belmoumen.meh@gmail.com>
2025-07-16 11:06:33 +0100

    add task 2 file

--- task_2.sql
+++ task_2.sql
@@ -1,7 +1,7 @@
 USE alx_book_store;
 
 -- Create the AUTHORS table
-CREATE TABLE IF NOT EXISTS authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
